29da82845a112963e21c78b9ed03103ea726bc1b
Ayush Mhaisane <ayushmhaisane25@gmail.com>
2025-11-19 13:30:20 +0530

    Update Layout.js

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -3,14 +3,11 @@ import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
 import './Layout.css';
 import Menus from '../Menus/Menus';
 
-// MODIFICATION: Removed 'Home' component from this file.
-// This component now ONLY renders the sidebar.
 
 const Layout = ({ toggle, handleToggle }) => {
   return (
     <>
       <div className='sidebar-section'>
-        {/* The sidebar class controls its width */}
         <div className={toggle ? "sidebar-toggle sidebar" :'sidebar'}>
             <div className='sidebar-toggle-icons'>
                 <p onClick={handleToggle}>
@@ -22,9 +19,7 @@ const Layout = ({ toggle, handleToggle }) => {
             <Menus toggle={toggle}/>
         </div>
         
-        {/* MODIFICATION: Removed the content wrapper and <Home /> component from here.
-            It is now in App.js. 
-        */}
+      
       </div>
     </>
   )
